b6fb35acef1f2aaae2600bebc55186132cb6864c
aishanirfan <aishanirfan6@gmail.com>
2024-07-05 14:17:36 +0530

    added more elements to the homepage

--- casesandcovers/src/app/page.tsx
+++ casesandcovers/src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -15,11 +16,52 @@ export default function Home() {
               <p className="mt-8 text-lg lg:pr1- max-w-prose text-center lg:text-left text-balance md:text-wrap">
                 Capture your favourite memories with your own, <span className="font-semibold">one-of-one</span> phone case. CasesandCovers allows you to protect your memories, not just you phone case.
               </p>
-              <ul className="mt-8">
-
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shirnk-0  text-green-600"/>
+                    High quality, durable material
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shirnk-0  text-green-600"/>
+                    5 year print guarantee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shirnk-0  text-green-600"/>
+                   Modern Iphone models supported
+                  </li>
+                </div>
               </ul>
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+                <div className="flex -space-x-4">
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-1.png" alt="user image"/>
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-2.png" alt="user image"/>
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-3.png" alt="user image"/>
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-4.jpg" alt="user image"/>
+                  <img className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-5.jpg" alt="user image"/>
+                </div>
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
+                  </div>
+                  <p>
+                    <span className="font-semibold">1300</span> happy customers
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
+
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <img src="/your-image.png" className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"/>
+            </div>
+          </div>
+
         </MaxWidthWrapper>
       </section>
     </div>
