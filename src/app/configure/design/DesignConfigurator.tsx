1c05f86830276024c4e4d875316995c92a2c8152
aishanirfan <aishanirfan6@gmail.com>
2024-07-11 21:14:05 +0530

    finished model select drop down

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -9,9 +9,9 @@ import {RadioGroup} from "@headlessui/react"
 import { useState } from "react"
 import { COLORS, MODELS } from "@/validators/option-validator"
 import { Label } from "@/components/ui/label"
-import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button"
-import { ChevronsUpDown } from "lucide-react"
+import { Check, ChevronsUpDown } from "lucide-react"
 
 interface DesignConfiguratorProps{
     configId : string,
@@ -98,7 +98,18 @@ const DesignConfigurator = ({configId, imageUrl, imageDimensions}: DesignConfigu
                                         <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                                     </Button>
                                 </DropdownMenuTrigger>
-                                
+                                <DropdownMenuContent>
+                                    {MODELS.options.map((model)=> (
+                                        <DropdownMenuItem key={model.label} className={cn("flex text-sm gap-1 items-center p-1.5 cursor-default hover:bg-zinc-100", {
+                                            "bg-zinc-100" :model.label === options.model.label
+                                        })} onClick={()=> {
+                                            setOptions((prev) => ({...prev, model}))
+                                        }}>
+                                            <Check className={cn("mr-2 h-4 w-4", model.label=== options.model.label ? "opacity-100" : "opacity-0")}/>
+                                            {model.label}
+                                        </DropdownMenuItem>
+                                    ))}
+                                </DropdownMenuContent>
                             </DropdownMenu>
                         </div>
                         </div>
