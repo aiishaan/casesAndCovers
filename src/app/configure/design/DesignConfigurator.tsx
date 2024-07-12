4ee6cc92c9d4ff90acfd1c8fe581b886dbd9af4e
aishanirfan <aishanirfan6@gmail.com>
2024-07-12 16:45:17 +0530

    styling changes for mobile view

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -33,7 +33,7 @@ const DesignConfigurator = ({configId, imageUrl, imageDimensions}: DesignConfigu
         material: MATERIALS.options[0],
         finish : FINISHES.options[0]
     })
-    return <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
+    return <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
         <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus: outline-none focus: ring-2 focus:ring-primary focus:ring-offset-2">
             <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
                 <AspectRatio ratio={896/ 1831} className="pointer-events-none relative z-50 aspect-[896/1831] w-full">
@@ -65,7 +65,7 @@ const DesignConfigurator = ({configId, imageUrl, imageDimensions}: DesignConfigu
         </div>
 
 
-        <div className="h-[37.5rem] flex flex-col bg-white">
+        <div className="h-[37.5rem] w-full col-span-full lg:col-span-1 flex flex-col bg-white">
             < ScrollArea className="relative flex-1 overflow-auto">
                 <div aria-hidden="true" className="absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none"/>
                 <div className="px-8 pb-12 pt-8">
