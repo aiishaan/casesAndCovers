f1803d32f4d23025d6a1bd4665c3e5bfa63f8c5a
aishanirfan <aishanirfan6@gmail.com>
2024-07-13 20:45:35 +0530

    added configured phone to the preview page

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -11,7 +11,7 @@ const Phone = ({imgSrc, className, dark = false, ...props} :PhoneProps) => {
         <div className={cn("relative pointer-events-none z-50 overflow-hidden", className)} {...props}>
             <img src={dark? "/phone-template-dark-edges.png": "/phone-template-white-edges.png"} className="pointer-events-none z-50 select-none" alt="phone img"/>
             <div className="absolute -z-10 inset-0">
-                <img src={imgSrc} className="object-cover" alt="overlaying phone image"/>
+                <img src={imgSrc} className="object-cover min-w-full min-h-full" alt="overlaying phone image"/>
             </div>
         </div>
     )
