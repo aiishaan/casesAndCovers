31cb32889ddea9b5fceb3042ec470e6e4ea4acb4
aishanirfan <aishanirfan6@gmail.com>
2024-07-15 17:04:04 +0530

    added the phone preview

--- src/components/PhonePreview.tsx
+++ src/components/PhonePreview.tsx
@@ -1,5 +1,22 @@
-const PhonePreview = () => {
-    return <div></div>
+"use client"
+
+import { CaseColor } from "@prisma/client"
+import { useRef, useState } from "react"
+import { AspectRatio } from "./ui/aspect-ratio"
+
+const PhonePreview = ({croppedImageUrl, color} : {croppedImageUrl: string, color: CaseColor}) => {
+
+    const ref = useRef<HTMLDivElement>(null)
+
+    const [renderedDimensions, setRenderedDimensions] = useState({
+        height: 0, width: 0
+    })
+
+    return <AspectRatio ref={ref} ratio={3000/2001} className="relative">
+        <div className="absolute z-20 scale-[1.0352]" style={{
+            left : renderedDimensions.width / 2 - renderedDimensions.width / (1216/ 121),
+        }}></div>
+    </AspectRatio>
 }
 
 export default PhonePreview
